e2679b940b7d5bf17e188967c9b7aa8d6a2496f8
Garrett Ellis <egarrett021@gmail.com>
2026-02-08 21:54:09 -0500

    updates

--- ItemDatabase.cpp
+++ ItemDatabase.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <string>
 
+//converts string weapon type into its enum form
 type getType(const string &str)
 {
     if (str == "Weapon") return Gun;
@@ -17,6 +18,7 @@ type getType(const string &str)
     return Gun;
 }
 
+//converts string weapon rarity type into its enum form
 rarity getRarity(const string &str)
 {
     if (str == "Common") return Common;
@@ -30,6 +32,7 @@ rarity getRarity(const string &str)
     return Common;
 }
 
+//converts string bullet type into its enum form
 bullet getBullet(const string &str)
 {
     if (str == "Light") return Light;
@@ -42,8 +45,10 @@ bullet getBullet(const string &str)
     return Light;
 }
 
+//method reading in the items' information from the CSV
 int ItemDatabase::readCSV()
 {
+    //variable declarations and initializations
     ifstream file;
     string strID, strRarityID, line, name, description, strAmmo, strMagSize, strDamage, strFireRate, strReloadTime, strType;
     int ID, magSize, i = 0;
@@ -52,14 +57,18 @@ int ItemDatabase::readCSV()
     double damage, fireRate, reloadTime;
     type typeID;
     Item *node;
+
+    //opening the file
     file.open("Items.csv");
 
+    //checking if the file opened successfully; otherwise, the program will terminate
     if (!file.is_open())
     {
         cerr << "Unable to open file." << endl;
         return 1;
     }
 
+    //reading each line of the file
     while (getline(file, line))
     {
         if (line.find_first_not_of(" \t\r\n") == string::npos)
@@ -67,42 +76,56 @@ int ItemDatabase::readCSV()
         
         stringstream ss(line);
 
+        //skipping the first line of the file
         if (i++ == 0)
             continue;
         
+        //reading in the item's ID and storing it as an int
         getline(ss, strID, ',');
         ID = stoi(strID);
 
+        //reading in the item's name
         getline(ss, name, ',');
 
+        //reading in the item's category type
         getline(ss, strType, ',');
         typeID = getType(strType);
 
+        //reading in the item's rarity type
         getline(ss, strRarityID, ',');
         rarityID = getRarity(strRarityID);
         
+        //reading in the item's description
         getline(ss, description, ',');
 
+        //if the item is not a weapon, instantiate a new node for the item
         if (typeID != Gun)
             node = new Item(ID, rarityID, name, description, typeID);
             
+        //otherwise if the item is a weapon
         else
         {
+            //reading in the weapon's ammo type and storing it as a bullet enum type
             getline(ss, strAmmo, ',');
             ammo = getBullet(strAmmo);
 
+            //reading in the weapon's damage value and storing it as a float
             getline(ss, strDamage, ',');
             damage = stod(strDamage);
 
+            //reading in the weapon's fire rate value and storing it as a float
             getline(ss, strFireRate, ',');
             fireRate = stod(strFireRate);
 
+            //reading in the weapon's magazine size and storing it as an int
             getline(ss, strMagSize, ',');
             magSize = stoi(strMagSize);
 
+            //reading in the weapon's reload time and storing it as a float
             getline(ss, strReloadTime, ',');
             reloadTime = stod(strReloadTime);
 
+            //instantiating a new Weapon node to be pushed back onto the vector of all items 
             node = new Weapon(ID, rarityID, name, description, typeID, damage, fireRate, reloadTime, ammo, magSize);
         }
 
@@ -112,17 +135,22 @@ int ItemDatabase::readCSV()
     return 0;
 }
 
+//method for defining the weapon's loot pool and item's loot pool
 void ItemDatabase::setLootPool()
 {
+    //variable declarations
     RarityWeight rarityWeight;
     double categoryWeight;
     Weapon *weapon;
 
+    //making sure the pools are empty before adding to them
     weaponPool.clear();
     itemPool.clear();
 
+    //looping through all items read from the file
     for (int i = 0; i < allItems.size(); i++)
     {
+        //switch statement to calculate the weight of the current item in the vector of all items
         switch (allItems[i]->getRarityID())
         {
             case 0: rarityWeight = WeightCommon; break;
@@ -135,10 +163,13 @@ void ItemDatabase::setLootPool()
             default: rarityWeight = WeightCommon; break;
         }
 
+        //checking if the current item in the vector is a weapon
         if (allItems[i]->getType() == Gun)
         {
+            //if so, convert the item into a Weapon pointer
             weapon = static_cast<Weapon *>(allItems[i]);
 
+            //switch statement needed to calculate the weight of the current weapon needed for the entry
             switch (weapon->getAmmo())
             {
                 case Light: categoryWeight = Weapons * 1.1; break;
@@ -149,11 +180,14 @@ void ItemDatabase::setLootPool()
                 default: categoryWeight = Weapons; break;
             }
 
+            //adding entry to the weapon pool
             weaponPool.addEntry(weapon, rarityWeight * categoryWeight);
         }
 
+        //otherwise if the item is not a weapon
         else
         {
+            //switch statement needed to calculate the weight of the current item needed for the entry
             switch (allItems[i]->getType())
             {
                 case Consumable: categoryWeight = Consumables; break;
@@ -162,17 +196,19 @@ void ItemDatabase::setLootPool()
                 default: categoryWeight = Weapons; break;
             }
 
+            //adding new entry into the item loot pool
             itemPool.addEntry(allItems[i], rarityWeight * categoryWeight);
         }
     }
 }
 
+//method for randomly choosing a weapon from the weapon loot pool
 Weapon *ItemDatabase::rollWeapon()
 {
     return static_cast<Weapon *>(weaponPool.roll());
 }
 
-//
+//method for randomly choosing an item from the item loot pool
 Item *ItemDatabase::rollItem()
 {
     return itemPool.roll();
